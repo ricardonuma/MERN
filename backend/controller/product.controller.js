b7a64db9dc91df0a3a4304a1db3a700d3a790584
Ricardo Numa <koki_numa@hotmail.com>
2024-09-17 17:30:47 -0700

    Add MERN project

---
+++ backend/controller/product.controller.js
@@ -0,0 +1,84 @@
+import Product from "../models/product.model.js";
+import mongoose from "mongoose";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.log("Error creating product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+
+  const product = req.body; // user will send this data
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+
+  if (!product.name && !product.price && !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide at least one field" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "Product updated",
+        data: updatedProduct,
+      });
+  } catch (error) {
+    console.log("Error updating product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    console.log("Error deleting product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
